800674b98428c8885327042cd801d1aa82552799
eshwagadkar <eshwagadkar007@gmail.com>
2023-11-04 17:11:42 +0530

    disable the defaults

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,12 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  rules: {
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'prettier/prettier': 'off',
+    'no-unused-vars': 'off',
+    'semi': 'off',
+    'eslint-disable-quotes': 'off',
+  },
 };
