c933ee63333a39f427d65a48afb4662ef4467654
eshwagadkar <eshwagadkar007@gmail.com>
2023-11-04 17:12:40 +0530

    re-write the App.js file

---
+++ App.js
@@ -0,0 +1,14 @@
+import {SafeAreaView, StyleSheet, Text} from 'react-native';
+
+const App = () => {
+return (
+    <SafeAreaView>
+    <Text>Hi</Text>
+</SafeAreaView>
+);
+}
+
+const styles = StyleSheet.create({
+});
+
+export default App;
