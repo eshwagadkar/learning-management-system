b73bbc798f53ee013050390b8d31e67b6b8701d0
eshwagadkar <eshwagadkar007@gmail.com>
2023-11-05 16:46:27 +0530

    tsx to js

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
-import {SafeAreaView, StyleSheet, Text} from 'react-native';
+import {SafeAreaView, StyleSheet, View, Text} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import Dashboard from './src/screens/Dashboard/Dashboard';
+import ListCourses from './src/screens/Courses/ListCourses';
+import CourseDetails from './src/screens/Courses/CourseDetails';
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
 return (
-    <SafeAreaView>
-    <Text>Hi</Text>
-</SafeAreaView>
-);
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Dashboard" component={Dashboard} />
+      </Stack.Navigator>
+    </NavigationContainer>
+    );
 }
 
 const styles = StyleSheet.create({
